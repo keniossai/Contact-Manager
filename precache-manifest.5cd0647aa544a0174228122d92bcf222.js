4953043bbe359bd551a050e6f604eee314150b62
Kenneth Ossai <kenossai@ymail.com>
2020-10-02 23:40:21 +0100

    Updates

---
+++ precache-manifest.5cd0647aa544a0174228122d92bcf222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790ba3fac21b9e9974ae3ab43b5652de",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "91338712d093c769e84c",
+    "url": "/contact-manager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d0f3604f524434b7dd4a",
+    "url": "/contact-manager/static/js/2.c15b2e62.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.c15b2e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91338712d093c769e84c",
+    "url": "/contact-manager/static/js/main.6f686880.chunk.js"
+  },
+  {
+    "revision": "ad86e1d3d1dedce9579f",
+    "url": "/contact-manager/static/js/runtime-main.e019823c.js"
+  }
+]);
